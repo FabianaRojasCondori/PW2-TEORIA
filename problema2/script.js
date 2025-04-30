7478fb5c177add7b0221f10199c422dcc39d8ee8
Fabiana Paola Rojas Condori <frojascon@unsa.edu.pe>
2025-04-30 16:50:12 -0500

    se creo y agrego archivo script.js para el problema 2.

---
+++ problema2/script.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+const data = require('./data.json');
+
+app.use(express.static(path.join(__dirname)));
+
+app.get('/', (request, response) => {
+    response.sendFile(path.resolve(__dirname,'index.html'));
+});
+
+app.listen(9090,() =>{
+    console.log("Escuchando en: http://localhost:9090")
+});
