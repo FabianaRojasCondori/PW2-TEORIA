4504f8086e9faabde0a253d13c18a14262e37829
Fabiana Paola Rojas Condori <frojascon@unsa.edu.pe>
2025-05-24 17:47:34 -0500

    Actualizando modelo Persona

--- Django/src/personas/models.py
+++ Django/src/personas/models.py
@@ -5,3 +5,11 @@ from django.db import models
 class Question(models.Model):
     question_text = models.CharField(max_length=200)
     pub_date = models.DateTimeField('date published')
+
+class Persona(models.Model):
+    nombre = models.CharField(max_length=100)
+    apellido = models.CharField(max_length=100)
+    edad = models.IntegerField()
+
+    def __str__(self):
+        return f"{self.nombre} {self.apellido}"
