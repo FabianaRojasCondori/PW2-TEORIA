d00896354a2d3d9a9747ce3f0e2251b55ed4daa9
Fabiana Paola Rojas Condori <frojascon@unsa.edu.pe>
2025-05-29 22:24:32 -0500

    Diapositiva 11: El argumento de la funciones de las vistas

--- Django2/src/inicio/views.py
+++ Django2/src/inicio/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render
 django.http import HttpResponse
 # Create your views here.
 def myHomeView(*args, **kwargs):
+    print(args, kwargs)
+    print(request.user)
     return HttpResponse("<h1>Hola Mundo desde DJango</h1>")
 def anotherView(request):
     return HttpResponse("<h1>Solo otra página.</h1>")
