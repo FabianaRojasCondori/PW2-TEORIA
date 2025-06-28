e9ee72b7495df697df40cb5bee7afae61881cb5e
FabianaRojasCondori <frojascon@unsa.edu.pe>
2025-06-28 18:53:30 -0500

    Django4 - Diapositiva N2:Se desarrollo el trabajo y se realizaron los primeros cambios indicados (agregar RawPersonaForm en forms.py y crear una vista asociada en views.py con una ruta registrada en urls.py

---
+++ Django4_5/src/personas/forms.py
@@ -0,0 +1,22 @@
+from django import forms
+
+from .models import Persona
+
+class PersonaForm(forms.ModelForm):
+    class Meta:
+        model = Persona
+        fields = [
+            'nombres',
+            'apellidos',
+            'edad',
+            'donador',
+        ]
+
+class RawPersonaForm(forms.Form):
+    nombres = forms.CharField()
+    apellidos = forms.CharField()
+    edad = forms.IntegerField()
+    donador = forms.BooleanField()
+                
+
+                
---
+++ Django4_5/src/personas/views.py
@@ -0,0 +1,28 @@
+from django.shortcuts import render
+from .models import Persona
+from .forms import PersonaForm, RawPersonaForm
+
+# Create your views here.
+def personaAnotherCreateView(request):
+    form = RawPersonaForm()
+    context = {
+        'form': form,
+    }
+    return render(request, 'personas/personasCreate.html', context)
+
+def personaTestView(request):
+    obj = Persona.objects.get(id = 1)
+    context = {
+        'objeto': obj,
+        }
+    return render(request, "personas/descripcion.html", context)
+
+def personaCreateView(request):
+    print('GET: ', request.GET)
+    print('POST: ', request.POST)
+    context = {}
+    return render(request, 'personas/personasCreate.html', context)
+
+def searchForHelp(request):
+    return render(request, "personas/search.html", {})
+
