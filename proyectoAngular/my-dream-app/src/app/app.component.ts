6c3586fb6d3fce575a6cb5e2ec180a9161521b6a
Fabiana Paola Rojas Condori <frojascon@unsa.edu.pe>
2025-07-05 17:51:19 -0500

    Angular1 - Diapositivas 13,14: Se agrego un constructor en app.component.ts y se usaron For e Ifs para mostrar los hobbies.

--- proyectoAngular/my-dream-app/src/app/app.component.ts
+++ proyectoAngular/my-dream-app/src/app/app.component.ts
@@ -1,16 +1,31 @@
+
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CommonModule],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'my-dream-app';
-  name = 'Fabiana Rojas C..';
-  email = 'frojascon@unsa.edu.pe';
-  webpage = 'http://www.unsa.edu.pe';
+    title = 'my-dream-app';
+    name : string;
+    email : string ;
+    webpage : string;
+    hobbies : string[] ;
+
+    constructor() {
+        console.log("Constructor working ... ");
+        this.name = "Fabiana Rojas C.";
+        this.email = "frojascon@unsa.edu.pe";
+        this.webpage = "http://www.unsa.edu.pe";
+        this.hobbies = ["Voley","Programación","Netflix"];
+    }
+
+    showhobbies () {
+        return true;
+    }
 }
