e5edfd40b9867514da87184c49457d653faa0d15
Fabiana Paola Rojas Condori <frojascon@unsa.edu.pe>
2025-07-05 19:58:57 -0500

    Angular 2 - Diapositivas 15,16,17: Insertando Ruteos /about y /hello al proyecto para navegar a traves de la url

--- proyectoAngular/my-dream-app/src/app/app.routes.ts
+++ proyectoAngular/my-dream-app/src/app/app.routes.ts
@@ -1,3 +1,9 @@
-import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+import { Routes } from '@angular/router';
+import { About } from './about/about';
+import { HelloWorld } from './hello-world/hello-world';
+
+export const routes: Routes = [
+  { path: 'about', component: About },
+  { path: 'hello', component: HelloWorld }
+];
